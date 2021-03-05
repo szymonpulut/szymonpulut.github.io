86aa2e9e0d52677b06ccd44bbccf3fc6a6a75bb8
Szymon Pulut <szymon.pulut@gmail.com>
2021-03-05 16:44:04 +0100

    Contact section updates

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,51 +4,48 @@ import PropTypes from 'prop-types';
 import styles from './Contact.module.scss';
 
 const Contact = ({ id }) => {
-    return (
-        <section className={styles.Contact} id={id}>
-            <header className={styles.Header}>contact & resume</header>
-            <article className={styles.Article}>
-                <p>
-                    {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
-                    my github profile:{' '}
-                    <a
-                        className={styles.Link}
-                        href="https://github.com/szymonpulut/"
-                    >
-                        github.com/szymonpulut
-                    </a>
-                </p>
-                <p>
-                    {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
-                    send me an email:{' '}
-                    <a href="mailto:szymon.pulut@gmail.com">
-                        szymon.pulut@gmail.com
-                    </a>
-                </p>
-                <p>
-                    {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
-                    download my{' '}
-                    <a className={styles.Link} href="pulut_szymon_cv.pdf">
-                        resume
-                    </a>
-                </p>
-                <p>
-                    {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
-                    I also have a{' '}
-                    <a
-                        className={styles.Link}
-                        href="https://www.linkedin.com/in/szymonpulut/"
-                    >
-                        LinkedIn profile
-                    </a>
-                </p>
-            </article>
-        </section>
-    );
+  return (
+    <section className={styles.Contact} id={id}>
+      <header className={styles.Header}>contact & resume</header>
+      <article className={styles.Article}>
+        <p>
+          {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
+          my github profile:{' '}
+          <a className={styles.Link} href="https://github.com/szymonpulut/">
+            github.com/szymonpulut
+            {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
+          </a>{' '}
+          (a bit inactive - last updates before working @ Tango)
+        </p>
+        <p>
+          {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
+          send me an email:{' '}
+          <a href="mailto:szymon.pulut@gmail.com">szymon.pulut@gmail.com</a>
+        </p>
+        <p>
+          {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
+          download my{' '}
+          <a className={styles.Link} href="pulut_szymon_cv.pdf">
+            resume
+          </a>
+        </p>
+        <p>
+          {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
+          I also have a{' '}
+          <a
+            className={styles.Link}
+            href="https://www.linkedin.com/in/szymonpulut/"
+          >
+            LinkedIn profile
+          </a>
+        </p>
+      </article>
+    </section>
+  );
 };
 
 Contact.propTypes = {
-    id: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
 };
 
 export default Contact;
