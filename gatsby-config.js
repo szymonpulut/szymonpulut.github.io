ac9cc2e862182f8d0a45499069e7a90eb01607e7
Szymon Pulut <szymon.pulut@gmail.com>
2022-01-04 18:35:58 +0100

    fix(gatsby-config): replaced all backticks with singlequotes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,35 +1,35 @@
 module.exports = {
   siteMetadata: {
-    title: `Szymon Pulut - Web Developer`,
-    description: `My personal webpage and portfolio - Szymon Pulut`,
-    author: `Szymon Pulut <szymon.pulut@gmail.com>`,
+    title: 'Szymon Pulut - Web Developer',
+    description: 'My personal webpage and portfolio - Szymon Pulut',
+    author: 'Szymon Pulut <szymon.pulut@gmail.com>',
   },
   plugins: [
     {
-      resolve: `gatsby-plugin-sass`,
+      resolve: 'gatsby-plugin-sass',
       options: {
         implementation: require('sass'),
       },
     },
-    `gatsby-plugin-dts-css-modules`,
-    `gatsby-plugin-image`,
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-dts-css-modules',
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `src`,
-        path: `${__dirname}/src/content`,
+        name: 'src',
+        path: '${__dirname}/src/content',
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: 'images',
+        path: '${__dirname}/src/images',
       },
     },
     {
-      resolve: `gatsby-transformer-remark`,
+      resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
           'gatsby-remark-relative-images',
@@ -43,21 +43,21 @@ module.exports = {
         ],
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/icon.svg`, // This path is relative to the root of the site.
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/icon.svg', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    // 'gatsby-plugin-offline',
   ],
 }
