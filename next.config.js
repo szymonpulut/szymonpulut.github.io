6543b59268a881b489b25ec370fd9fd306df64a0
Szymon Pulut <szymon.pulut@gmail.com>
2023-08-01 22:52:26 +0200

    fix: ci/cd (#33)
    * fix(eslint): add next config to ignores of eslint and prettier
    
    * fix(eslint): remove unnecessary eslint extension
    
    * fix(posts): add an empty posts directory to prevent building errors
    
    * fix(typescript): add optional chaining
    
    * fix(nextjs): remove unnecessary config
    
    * fix(posts api): only parse .md files

--- next.config.js
+++ next.config.js
@@ -1,22 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
 const nextConfig = {
   pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
-  assetPrefix,
-  basePath,
 }
 
 module.exports = nextConfig
