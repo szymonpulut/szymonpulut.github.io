df79534d9de46cffcc16f3da2a1b8ae63c25f42f
Szymon Pulut <szymon.pulut@gmail.com>
2023-08-01 17:50:00 +0200

    test888

--- next.config.js
+++ next.config.js
@@ -1,22 +1,6 @@
 /** @type {import('next').NextConfig} */
-
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = ''
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
 const nextConfig = {
   pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
-  assetPrefix: assetPrefix,
-  basePath: basePath,
 }
 
 module.exports = nextConfig
