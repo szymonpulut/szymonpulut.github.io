b559234700c69ff8839e4ac405ea079e1c4572d7
Szymon Pulut <szymon.pulut@gmail.com>
2023-08-01 22:57:37 +0200

    feat(fonts): use fonts locally cached during ssg

--- app/rootLayout.component.tsx
+++ app/rootLayout.component.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Lato } from 'next/font/google'
 import Head from 'next/head'
 
 import HeaderComponent from '@/src/components/Header/Header.component'
@@ -7,10 +8,13 @@ import useBodyScrollLock from '@/src/hooks/useBodyScrollLock.hook'
 import useEffectInWindow from '@/src/hooks/useEffectInWindow.hook'
 import useIsPageScrolled from '@/src/hooks/useIsPageScrolled.hook'
 import useSmoothChangeThemeColorOnScroll from '@/src/hooks/useSmoothChangeThemeColorOnScroll.hook'
+import globalStyleVariables from '@/src/styles/globalVariables.module.scss'
 
 import { ContextProviders } from './contextProviders'
 
 import 'src/styles/globalStyles.scss'
+
+const latoFont = Lato({ weight: '400', subsets: ['latin'], display: 'swap' })
 
 interface RootLayoutComponentProps {
   children: React.ReactNode
@@ -46,6 +50,12 @@ const RootLayoutComponent: React.FC<RootLayoutComponentProps> = ({
         <meta charSet="UTF-8" />
 
         <link rel="icon" type="image/x-icon" href="/icon.svg" />
+
+        <style jsx global>{`
+          html {
+            font-family: ${latoFont.style.fontFamily};
+          }
+        `}</style>
       </Head>
 
       <FullScreenNavigation
