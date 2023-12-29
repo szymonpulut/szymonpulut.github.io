5a3fcd57070791054ea066e73fd7704922e1f503
Szymon Pulut <szymon.pulut@gmail.com>
2023-12-29 13:07:59 +0100

    refactor: minor fixes in rootLayout and NavigationItem (#69)
    * refactor(root layout): remove unnecessary useEffects and state pieces
    
    These mostly derived state from other stateful elements.
    
    * refactor(root layout): change path-style in css import
    
    * refactor(navigation item): added whitespaces in classNames for easier readability

--- app/rootLayout.component.tsx
+++ app/rootLayout.component.tsx
@@ -7,12 +7,11 @@ import FullScreenNavigation from '@/src/components/Navigation/FullScreenNavigati
 import useBodyScrollLock from '@/src/hooks/useBodyScrollLock.hook'
 import useEffectInWindow from '@/src/hooks/useEffectInWindow.hook'
 import useSmoothChangeThemeColorOnScroll from '@/src/hooks/useSmoothChangeThemeColorOnScroll.hook'
-import globalStyleVariables from '@/src/styles/globalVariables.module.scss'
 
 import { ContextProviders } from './contextProviders'
 import { latoFont } from './fonts'
 
-import 'src/styles/globalStyles.scss'
+import '@/src/styles/globalStyles.scss'
 
 interface RootLayoutComponentProps {
   children: React.ReactNode
@@ -26,19 +25,12 @@ const RootLayoutComponent: React.FC<RootLayoutComponentProps> = ({
   const [isOpenFullScreenNavigation, setIsOpenFullScreenNavigation] =
     useState(false)
 
-  const dynamicThemeColor = useSmoothChangeThemeColorOnScroll()
-  const [themeColor, setThemeColor] = useState(
-    globalStyleVariables.backgroundColor,
-  )
-
-  useEffectInWindow(() => {
-    if (dynamicThemeColor) {
-      setThemeColor(dynamicThemeColor)
-    }
-  }, [dynamicThemeColor])
+  const themeColor = useSmoothChangeThemeColorOnScroll()
 
   const { lockScroll, unlockScroll } = useBodyScrollLock()
 
+  // Technically lockScroll & unlockScroll could've been outside useEffect
+  // But since this is a side-effect, I left it inside
   useEffectInWindow(() => {
     if (isOpenFullScreenNavigation) {
       lockScroll()
