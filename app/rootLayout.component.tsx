7578392b1264a3d98ba47ee81a95e56adac10c83
Szymon Pulut <szymon.pulut@gmail.com>
2023-12-29 23:19:16 +0100

    Revert "fix(lato font): move lato font fetching to root layout component (#76)"
    This reverts commit 86fa46af74853bac327458852b0c39dc6c468954.

--- app/rootLayout.component.tsx
+++ app/rootLayout.component.tsx
@@ -49,18 +49,6 @@ const RootLayoutComponent: React.FC<RootLayoutComponentProps> = ({
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
-        <link
-          href="https://fonts.googleapis.com/css2?family=Lato:wght@400;900&display=swap"
-          rel="stylesheet"
-        />
-
         <link rel="icon" type="image/x-icon" href="/icon.svg" />
 
         <link
