627d90eccbbea1cca2b1b1f886607732b0e37d8a
Szymon Pulut <szymon.pulut@gmail.com>
2024-02-14 14:01:55 +0100

    fix(og links): fix published date

--- app/rootLayout.component.tsx
+++ app/rootLayout.component.tsx
@@ -83,10 +83,13 @@ const RootLayoutComponent: React.FC<RootLayoutComponentProps> = ({
         <meta name="author" content="Szymon Pulut" />
 
         {articlePublishedDate && (
-          <meta
-            property="article:published_time"
-            content={articlePublishedDate}
-          />
+          <>
+            <meta
+              property="article:published_time"
+              content={articlePublishedDate}
+            />
+            <time dateTime={articlePublishedDate} />
+          </>
         )}
 
         <meta name="theme-color" content={themeColor} />
